e6021e4f53c44b80ce75fcc1f410cac65458bccb
fixjsl <dkd462@gmail.com>
2024-11-21 02:14:19 +0900

    Add project files.

---
+++ Project1/Header.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <cstring> 
+#include <cstdint>
+#include <chrono> 
+#include <thread>
+
+
+
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#pragma comment(lib, "ws2_32.lib")
+
+
+
+#elif defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+
+#endif
